45dbb28a416058ce13f09d13cd95727638d299a1
MeliAlvarez14 <melialvarez180@gmail.com>
2024-12-24 02:42:16 -0300

    estilos de la página del carrito y funcionamiento

--- scripts/productos.js
+++ scripts/productos.js
@@ -1,17 +1,18 @@
 //Comportamiento de la página
 visualizarProductos();
-//
 
-//let elementos;
+
+
+let productos;
 
 async function visualizarProductos(){
     try{
         const response = await fetch('./scripts/listaProductos.json');
-        const array = await response.json();
+        productos = await response.json();
 
         const listaProductos = document.querySelector("#listaProductos");
         listaProductos.innerHTML = "";
-        array.forEach((item) => {
+        productos.forEach((item) => {
             const productos = crearProducto(item);
             listaProductos.innerHTML += productos;
         });
@@ -25,7 +26,7 @@ async function visualizarProductos(){
 
 function crearProducto(item){
     const html = `
-        <article class="${item.class}" data-id="${item.id}">
+        <article data-id="${item.id}">
             <img src="./img/${item.image}" alt="${item.producto}">
             <p>${item.producto}</p>
             <h3>$${item.precio}</h3>
@@ -34,5 +35,116 @@ function crearProducto(item){
         `
     return html;
 };
+
+
+//Carrito de compras
+const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    cargarCarrito();
+});
+
+document.addEventListener("click", (event) => {
+    if(event.target.classList.contains("agregarProducto")){
+        const idt = event.target.closest('article').dataset.id;
+        const elemento = productos.find((producto) => producto.id == idt);
+
+        const {id, image, producto, precio} = elemento;
+        const item = {
+            id: id,
+            image: image,
+            producto: producto,
+            precio: precio
+        };
+        carrito.push(item);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        cargarCarrito();
+    }
+
+    //Eliminar producto
+    if(event.target.classList.contains("eliminar")){
+        const id = event.target.closest('.itemsCarrito').dataset.id;
+        let nuevo = carrito.filter((producto) => producto.id != id);
+
+        localStorage.setItem("carrito", JSON.stringify(nuevo));
+        cargarCarrito();
+    }
+    
+});
+
+
+function cargarCarrito() {
+    const itemsCarrito = document.querySelector("#comprasCarrito");
+    
+    if (carrito.length === 0) {
+        itemsCarrito.innerHTML = "<h2>No hay productos en tu carrito</h2>";
+    }
+    else{
+        itemsCarrito.innerHTML = "";
+    
+        carrito.forEach((item) => {
+            const html = `
+                    <div class="itemsCarrito" data-id="${item.id}">
+                        <div class="presentacion">
+                            <img src="./img/${item.image}" alt="${item.producto}">
+                        </div>
+                        <div class="selectoresCantidad">
+                            <p>${item.producto}</p>
+                            <div class="botones">
+                                <button class="disminuir">-</button>
+                                <p class="cantidadProductos"></p>
+                                <button class="aumentar">+</button>
+                            </div>                            
+                        </div>
+                        <div class="eliminarYPrecio">
+                            <p>$${item.precio}</p>
+                        <button class="eliminar">Eliminar</button>
+                        </div>
+                    </div>
+            `;
+
+            itemsCarrito.innerHTML += html;
+    });
+
+    }
+}
+
+
+
+
+
+
+/*
+//Detalle de carrito
+const itemsCarrito = document.querySelector("#comprasCarrito");
+
+itemsCarrito.innerHTML = "";
+
+carrito.forEach((item) => {
+    const html = `
+        <div class="itemsCarrito" data-id="${item.id}">
+            <img src="./img/${item.image}" alt="${item.producto}">
+            <p>${item.producto}</p>
+            <div class="botones">
+                <div class="selectoresCantidad">
+                    <button class="disminuir">-</button>
+                    <p class="cantidadProductos"></p>
+                    <button class="aumentar">+</button>
+                </div>
+                <button class="eliminar">Eliminar</button>
+            </div>
+            <p>$${item.precio}</p>
+        </div>
+    `;
+
+    itemsCarrito.innerHTML += html;
+});
+*/
+
+
+
+
+
+
 
 
